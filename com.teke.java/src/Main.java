389b0354ae509381fe8d47e907a9da30f98b824c
teketel <for.teketel@gmail.com>
2014-05-15 16:09:44 +0300

    test fail

--- com.teke.java/src/Main.java
+++ com.teke.java/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
   public static class AntTest {
      public int multi(int number1, int number2) {
-        return number1 * number2;
+        return number1 * number2333;
      }
   }
 }
